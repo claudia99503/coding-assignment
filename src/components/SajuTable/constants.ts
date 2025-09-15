103b02f76f8b59dc70cc4a6febd029d7bf5a7b94
minseokim <claudia503@naver.com>
2025-09-15 22:53:48 +0900

    feat: 사주표 레이아웃/디자인 상수 추가

---
+++ src/components/SajuTable/constants.ts
@@ -0,0 +1,12 @@
+export const LAYOUT = {
+  columnRawWidths: [47.92, 65.52, 65.53, 65.52, 65.52],
+  rowHeights: [44.99, 42.05, 66.5, 66.5, 44.28, 44.28, 44.28, 120.96],
+} as const;
+
+export const DESIGN_WIDTH_PX = 375;
+export const DESIGN_HEIGHT_PX = 32 + 92 + 473;
+
+export const MAIN_LINE_PX = 21;
+export const SUB_LINE_PX = 10;
+export const GAP_BELOW_MAIN_PX = 1.04;
+
